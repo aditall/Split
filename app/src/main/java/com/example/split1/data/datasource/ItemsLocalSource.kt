777ef9fce1a4d0940bee27dbcaab3c39b50ca944
hubhe <adital31328@gmail.com>
2024-05-06 22:05:22 +0300

    Added data logics

---
+++ app/src/main/java/com/example/split1/data/datasource/ItemsLocalSource.kt
@@ -0,0 +1,29 @@
+package com.example.split1.data.datasource
+
+import com.example.split1.data.database.items.ItemsDao
+import com.example.split1.data.model.RoomItem
+
+class ItemsLocalSource (private val itemsDao: ItemsDao) {
+    fun getAllItems(): List<RoomItem> {
+        return itemsDao.getAllItems()
+    }
+
+    fun insert(space: RoomItem) {
+        itemsDao.insert(space)
+    }
+
+    fun getItem(spaceId: String): RoomItem {
+        return itemsDao.get(spaceId)
+    }
+
+    fun getMyItems(userId: String): List<RoomItem> {
+        return itemsDao.getMyItems(userId)
+    }
+
+    fun deleteByIds(spaces: List<RoomItem>) {
+        for (space in spaces) {
+            itemsDao.delete(space)
+        }
+    }
+
+}
